d24e08c6ca915f66f007302eade01184884f8d32
Chuck Shotton <cshotton@gmail.com>
2021-09-20 14:25:57 -0400

    Added skeleton scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "This is where the magic happens!"
+echo "Starting truebit-eth-server using $TBMODE mode."
+
+# copy binaries from external volume to internal storage
+# examine env vars and run the proper start-up script
+case "$TBMODE" in
+    init)
+        bash /tbscripts/init.sh
+        ;;
+
+    solver)
+        bash /tbscripts/solver.sh
+        ;;
+    
+    verifier)
+        bash //tbscripts/verifier.sh
+        ;;
+
+    *)
+        echo "No TBMODE environment variable found. Running interactive init mode."
+        bash /tbscripts/init.sh
+esac
