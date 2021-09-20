18b79c8f08358b8ddd97552c11dbfaa9f4abcc41
Evin Grano <evin.grano@gmail.com>
2021-09-20 17:03:05 -0400

    [WIP] code for the solver/verifier

---
+++ scripts/goerli_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Initialize Truebit toolchain for generating C/C++ tasks
+#source /emsdk/emsdk_env.sh #(run this first)
+sed -i "s|EMSCRIPTEN_NATIVE_OPTIMIZER = emsdk_path + '/fastcomp-clang/e1.37.36_64bit/optimizer'|EMSCRIPTEN_NATIVE_OPTIMIZER = ''|" /emsdk/.emscripten
+sed -i "s|LLVM_ROOT = emsdk_path + '/fastcomp-clang/e1.37.36_64bit'|LLVM_ROOT = '/usr/bin'|" /emsdk/.emscripten
+#emcc -v
+
+# Variables
+CLEF_IPC='/root/.clef/clef.ipc'
+GETH_IPC='/root/.ethereum/goerli/geth.ipc'
+
+# Refresh Clef and Geth IPC sockets
+rm $CLEF_IPC &>/dev/null
+rm $GETH_IPC &>/dev/null
+
+# Start IPFS
+ipfs init &>/dev/null
+tmux new -d 'ipfs daemon'
+
+# Start Clef and Geth
+GETH=$(echo 'nohup geth --rpc --nousb --goerli --syncmode light --signer' $CLEF_IPC)
+cat <<< $(jq '.geth.providerURL="/root/.ethereum/goerli/geth.ipc"' /truebit-eth/wasm-client/config.json) > /truebit-eth/wasm-client/config.json
+CLEF_CMD='clef --advanced --nousb --chainid 5 --keystore ~/.ethereum/goerli/keystore --rules /truebit-eth/wasm-client/ruleset.js'
+screen -d -m -S ClefSession
+screen -S ClefSession -p 0 -X stuff "stty -echo^M"
+screen -S ClefSession -p 0 -X stuff "$CLEF_CMD^M"
+sleep 0.25
+screen -S ClefSession -p 0 -X stuff "ok^M$CLEF_PWD^M"
+until [ -S $CLEF_IPC ]; do sleep 0.1; done;
+$GETH 2>> ~/logs/geth_session.log &
--- scripts/verifier.sh
+++ scripts/verifier.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "verifier mode."
+
+bash ./goerli_setup.sh &
+
+WALLET=${SOLVER_WALLET_ID:=0}
+
+echo "Waiting for GETH IPC to Exist..."
+until [ -S $GETH_IPC ]; do sleep 0.1; done
+cd /truebit-eth
+echo "Starting TrueBit-OS In Verifier Mode..."
+./truebit-os -c 'start solve -a $WALLET' --batch 2>> ~/logs/truebit-solver.log &
